bb1579e3504448b594507f625798624d71621bc1
Isay Konter <isaykonter@gmail.com>
2018-12-07 11:33:34 +0100

    Update video_to_frames.py
    Added: Grayscale and saving pixel values in text document.

--- video_to_frames.py
+++ video_to_frames.py
@@ -1,10 +1,24 @@
 import cv2
 import os
+import numpy as np
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
+    
+def calculate_array_values(image_array):
+    current_array = image_array
+    smallest_value = 255
+    largest_value = 0
+    for i in current_array:
+        if i < smallest_value:
+            smallest_value = i
+        if i > largest_value:
+            largest_value = i
+    mean = sum(current_array) / len(current_array)
+    return mean, smallest_value, largest_value
 
-def gcd(x, y): # Calculate greatest common devider.
+
+def gcd(x, y):  # Calculate greatest common devider.
     while y != 0:
         (x, y) = (y, x % y)
     return x
@@ -23,10 +37,13 @@ def video_capture(videolink, frameinterval):
     while success:
         if count % framecap == 0:  # check if the framecount can be devided by 300
             cropped = image[upper_border:height, 0:width]
-            # gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
             cv2.imwrite("frames/%s_frame%d.jpg" % (name, count / framecap), cropped)  # save image every 300th frame
-            split_image("frames/%s_frame%d.jpg" % (name, count / framecap), 480, 720)
+            # split_image("frames/%s_frame%d.jpg" % (name, count / framecap), 480, 720)
 
+            gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+
+            cv2.imwrite("frames/gray_%s_frame%d.jpg" % (name, count / framecap), gray)  # save image every 300th frame
+            split_image("frames/gray_%s_frame%d.jpg" % (name, count / framecap), 480, 720)
         success, image = vidcap.read()
         print("read a new frame succes:", success)
         count += 1
@@ -38,14 +55,20 @@ def split_image(image_source, height, width): # load image and split it into squ
     divider = gcd(height, width)
     y_length = int(height / divider)
     x_length = int(width / divider)
-    print(y_length)
-    print(x_length)
+
     for x in range(x_length):
         for y in range(y_length):
             y_start = y * divider
             x_start = x * divider
             segment = image[y_start:(y_start + divider), x_start:(x_start + divider)]
             cv2.imwrite("samples/%s_segment(%d,%d).jpg" % (name, x, y), segment)
+            b, g, r = cv2.split(segment)
+            b = np.array(b * 255)
+            b = b.flatten()
+
+            with open("data/%s_segment(%d,%d)_arrays.txt" % (name, x, y), 'a') as f:
+                np.savetxt(f, b, fmt='%i', delimiter='\t')
+                f.close()
 
 
 Tk().withdraw() # Simple GUI to select a file
@@ -57,3 +80,4 @@ elif filename.endswith(".mp4"):
     video_capture(filename, 300)
 else:
     print("invalid extension")
+
